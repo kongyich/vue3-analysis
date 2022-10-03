4991d9c3fa9e06ece4bc651443d25dc95710b212
“keyie” <570588655@qq.com>
2022-10-03 14:28:25 +0800

    :sparkles: add transform

---
+++ compiler/4.实现解析三种联合类型/parse.js
@@ -0,0 +1,152 @@
+const baseParse = function(content) {
+  const context = createParserContext(content)
+  return createRoot(parseChildren(context, []))
+}
+
+const parseChildren = function(context, ancestors) {
+  const nodes = []
+
+  while(!isEnd(context, ancestors)) {
+    let node;
+    let s = context.source
+    if(s.startsWith("{{")) {
+      node = parseInterpolation(context)
+    } else if(s[0] === '<') {
+      if(/[a-z]/i.test(s[1])) {
+        node = parseElement(context, ancestors)
+      }
+    }
+
+    if(!node) {
+      node = parseText(context)
+    }
+
+    nodes.push(node)
+  }
+
+  return nodes
+}
+
+const isEnd = function(context, ancestors) {
+  const s = context.source
+  if(s.startsWith("</")) {
+    let tag = ancestors[ancestors.length - 1].tag
+    if(startsWithEndTagOpen(s, tag)) {
+      return true
+    }
+    // for(let i = ancestors.length - 1; i >= 0; i--) {
+    //   const tag = ancestors[i].tag
+    //   if(startsWithEndTagOpen(s, tag)) {
+    //     return true
+    //   }
+    // }
+  }
+  return !s
+}
+
+const startsWithEndTagOpen = function(source, tag) {
+  return source.startsWith('</') && source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase()
+}
+
+
+const parseText = function(context) {
+  let endIndex = context.source.length
+  let endTokens = ['<', '{{']
+
+  for(let i = 0; i < endTokens.length; i++) {
+    let index = context.source.indexOf(endTokens[i])
+    if(index !== -1 && endIndex > index) {
+      endIndex = index
+    }
+  }
+
+  const content = parseTextData(context, endIndex)
+
+  return {
+    type: NodeType.TEXT,
+    content,
+  }
+}
+
+const parseTextData = function(context, length) {
+  const content = context.source.slice(0, length)
+
+  // 推进
+  advanceBy(context, length)
+  return content
+}
+
+const TagType = {
+  START: "start",
+  END: "end"
+}
+
+const parseElement = function(context, ancestors) {
+  const element = parseTag(context, TagType.START)
+  ancestors.push(element)
+  element.children = parseChildren(context, ancestors);
+  ancestors.pop()
+
+  if(startsWithEndTagOpen(context.source, element.tag)) {
+    // 解析闭合标签
+    parseTag(context, TagType.END)
+  } else {
+    throw new Error(`缺少结束标签：${element.tag}`)
+  }
+  return element
+}
+
+const parseTag = function(context, type) {
+  const match = /^<\/?([a-z]*)/i.exec(context.source)
+  const tag = match[1]
+  advanceBy(context, match[0].length)
+  advanceBy(context, 1)
+  if(type === TagType.END) return
+
+  return {
+    type: NodeType.ELEMENT,
+    tag
+  }
+}
+
+
+const parseInterpolation = function(context) {
+  const openDelimiter = "{{"
+  const closeDelimiter = "}}"
+
+  const closeIndex = context.source.indexOf(closeDelimiter, openDelimiter.length)
+
+  advanceBy(context, openDelimiter.length)
+  const rawContentLength = closeIndex - closeDelimiter.length
+  const rawContent = parseTextData(context, rawContentLength)
+
+  const content = rawContent.trim()
+
+  advanceBy(context, closeDelimiter.length)
+
+  return {
+    type: NodeType.INTERPOLATION,
+    content: {
+      type: NodeType.SIMPLE_EXPRESSION,
+      content: content
+    }
+  }
+}
+
+const advanceBy = function(context, length) {
+  context.source = context.source.slice(length)
+}
+
+const createRoot = function(children) {
+  return {
+    children
+  }
+}
+
+const createParserContext = function(context) {
+  return {
+    source: context
+  }
+}
+
+
